89d566c1e3abcd4335cceaa82352e0c57d7539bb
jojimanos <manosgeorge7@gmail.com>
2023-08-08 22:54:31 +0300

    exercise 4.7

--- exercises/blogserver/test/mostLikes.test.js
+++ exercises/blogserver/test/mostLikes.test.js
@@ -55,12 +55,8 @@ describe('most likes test', () => {
 
         const result = mostLikesHelper.mostLikes(blogs)
         expect(result).toEqual({
-                _id: "5a422b3a1b54a676234d17f9",
-                title: "Canonical string reduction",
-                author: "Edsger W. Dijkstra",
-                url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-                likes: 12,
-                __v: 0
+                name: "Edsger W. Dijkstra",
+                likes: 17,
             })
     })
 })
--- exercises/blogserver/utils/mostLikes_helper.js
+++ exercises/blogserver/utils/mostLikes_helper.js
@@ -1,13 +1,85 @@
 const mostLikes = (blogs) => {
 
-    const likes = blogs.map(blog => blog.likes)
-    console.log(Math.max(...likes))
+    const authorsInstances = blogs.map(blog => blog.author)
 
-    const mostLikes = blogs.filter((blog) => {return blog.likes === Math.max(...likes)})
-    console.log(mostLikes)
+    const authors = [...new Set(authorsInstances)]
 
-    return mostLikes[0]
+    const authorTupples = authors.map(author => { return { name: author, likes: 0 } })
+
+    authorTupples
+        .map(author => {
+            return blogs
+                .map( blog =>
+                    {return blog.author === author.name
+                        ? 
+                        author.likes =
+                        author.likes + blog.likes
+                        :
+                        author.likes
+                    })
+        })
+
+    const likes = authorTupples.map(author => author.likes)
+
+    const maxLikes = Math.max(...likes)
+
+    const response = authorTupples.filter(author => author.likes === maxLikes)
+
+    console.log(authors, authorTupples, likes, maxLikes, response)
+
+    return response[0]
 
 }
 
-module.exports = {mostLikes}
+console.log(mostLikes([
+    {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+    },
+    {
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+        __v: 0
+    },
+    {
+        _id: "5a422bc61b54a676234d17fc",
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2,
+        __v: 0
+    }
+]))
+
+module.exports = { mostLikes }
